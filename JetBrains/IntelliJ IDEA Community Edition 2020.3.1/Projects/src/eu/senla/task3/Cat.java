27d3399b8bdb335794a8aa6b9531b047a1163da9
Alexei Kostin <shopautogrodno@gmail.com>
2021-01-27 00:02:22 +0300

    Commit Task3

---
+++ JetBrains/IntelliJ IDEA Community Edition 2020.3.1/Projects/src/eu/senla/task3/Cat.java
@@ -0,0 +1,64 @@
+package eu.senla.task3;
+
+public class Cat {
+    String name = "Mersik";
+    int age = 5;
+    public void Cat1() {
+
+        int i = 0;
+        while (i < 10) {
+            i++;
+            System.out.println(name + " " + age);
+        }
+    }
+    public void getsetCat () {
+
+    }
+        public void setName (String name){
+            this.name = name;
+        }
+        public String getName () {
+            return name;
+        }
+        public void setAge ( int age){
+            this.age = age;
+        }
+        public int getAge () {
+            return age;
+        }
+
+        public void KitCat (String name, int age) {
+            this.name = name;
+            this.age = age;
+            int i = 0;
+            do {
+                i++;
+                System.out.println(name + " " + age);
+            }
+            while (i < 10);
+        }
+            public void Tom() {
+                String name = "Tom";
+                int age = 10;
+                int [] array = new int[5];
+                for (int i: array)
+                      {
+                          System.out.println(name + " " + age);
+                    
+                }
+
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
---
+++ JetBrains/IntelliJ IDEA Community Edition 2020.3.1/Projects/src/eu/senla/task3/Main.java
@@ -0,0 +1,21 @@
+package eu.senla.task3;
+
+public class Main {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.Cat1();
+
+              cat.setAge(7);
+              cat.getAge();
+              int cat2age = cat.getAge();
+              cat.setName("Barsik");
+              String cat2Name = cat.getName();
+                   for (int i = 0; i < 10; i++) {
+                   System.out.println(cat2Name + " " + cat2age);
+        }
+                  Cat Maik = new Cat();
+                  Maik.KitCat("Maik", 3);
+                  Cat Tom = new Cat();
+                  cat.Tom();
+    }
+}
