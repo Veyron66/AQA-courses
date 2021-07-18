71848834e0686d210013ad32aef9a4639403def0
Alexei Kostin <shopautogrodno@gmail.com>
2021-07-18 17:43:30 +0300

    First Test

--- Javadev/ws/Project1/src/test/java/FirstTest.java
+++ Javadev/ws/Project1/src/test/java/FirstTest.java
@@ -67,6 +67,7 @@ public class FirstTest {
         // Выход из системы
         driver.findElement(By.xpath("//a[@data-test='sign-out']")).click();
 
+
         Thread.sleep(5000);
         driver.close();
         driver.quit();
